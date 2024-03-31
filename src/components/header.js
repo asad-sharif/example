6511a312928f6ff66677c0a22720a73fc73af000
Asad Sharif <asadsharif1270@gmail.com>
2024-03-31 22:30:55 +0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    const styles = {
+        theme: {
+            backgroundColor: 'black',
+            color: 'pink'
+        }
+    }
+    return (
+        <h1 style={styles.theme}>Learning React is fun 😀</h1>
+    )
+}
